9e66964381e496fb609d7bcd5c8599e8d4cc8098
Seungchul Ha <seungchul0406@gmail.com>
2018-11-06 22:23:46 +0900

    scan_publisher added

--- dualarm_mobile_bringup/src/scan_publisher.cpp
+++ dualarm_mobile_bringup/src/scan_publisher.cpp
@@ -1,20 +1,20 @@
 #include "ros/ros.h"
 #include <sensor_msgs/LaserScan.h>
 
-int64_t scan_msg = 0;
+sensor_msgs::LaserScan scan_ori;
 
 void scanCallback(const sensor_msgs::LaserScan::ConstPtr& scan)
 {
-	fram_id=scan->header.frame_id
-	angle_min=scan.angle_min;
-    angle_max=scan.angle_max;
-	angle_increment=scan.angle_increment;
-	scan_time=scan.scan_time;
-    time_increment=scan.time_increment;
-    range_min=scan.range_min;
-    range_max=scan.range_max;
-	intensities=scan.intensities;
-    ranges=scan.ranges;
+	scan_ori.header.frame_id = scan->header.frame_id;
+	scan_ori.angle_min = scan->angle_min;
+    scan_ori.angle_max = scan->angle_max;
+	scan_ori.angle_increment = scan->angle_increment;
+	scan_ori.scan_time = scan->scan_time;
+    scan_ori.time_increment = scan->time_increment;
+    scan_ori.range_min = scan->range_min;
+    scan_ori.range_max = scan->range_max;
+	scan_ori.intensities = scan->intensities;
+    scan_ori.ranges = scan->ranges;
 }
 
 int main(int argc, char **argv)
@@ -24,11 +24,23 @@ int main(int argc, char **argv)
 
 	ros::Subscriber measure_sub = nh.subscribe("/scan", 100, scanCallback);
 	ros::Publisher scan_pub = nh.advertise<sensor_msgs::LaserScan>("/scan_revised", 100);
-	}
 	
-	scan_pub.publish(scan);
-		ros::spinOnce();
-		loop_rate.sleep();
+	while(ros::ok())
+	{
+		sensor_msgs::LaserScan scan_revised;
+		scan_revised.header.stamp = ros::Time::now();
+		scan_revised.header.frame_id = scan_ori.header.frame_id;
+		scan_revised.angle_min = scan_ori.angle_min;
+		scan_revised.angle_increment = scan_ori.angle_increment;
+		scan_revised.scan_time = scan_ori.scan_time;
+		scan_revised.time_increment = scan_ori.time_increment;
+		scan_revised.range_min = scan_ori.range_min;
+		scan_revised.range_max = scan_ori.range_max;
+		scan_revised.intensities = scan_ori.intensities;
+		scan_revised.ranges = scan_ori.ranges;
+		scan_pub.publish(scan_revised);
+		// ros::spinOnce();
+		// loop_rate.sleep();
+	}
 	return 0;
-
 }
