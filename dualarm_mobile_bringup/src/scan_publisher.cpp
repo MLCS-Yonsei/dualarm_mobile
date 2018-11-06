673219d8ad744bc6b612db84ea0a42f782c003ff
Seungchul Ha <seungchul0406@gmail.com>
2018-11-06 23:18:57 +0900

    scan publisher revised

--- dualarm_mobile_bringup/src/scan_publisher.cpp
+++ dualarm_mobile_bringup/src/scan_publisher.cpp
@@ -7,14 +7,14 @@ void scanCallback(const sensor_msgs::LaserScan::ConstPtr& scan)
 {
 	scan_ori.header.frame_id = scan->header.frame_id;
 	scan_ori.angle_min = scan->angle_min;
-    scan_ori.angle_max = scan->angle_max;
+	scan_ori.angle_max = scan->angle_max;
 	scan_ori.angle_increment = scan->angle_increment;
 	scan_ori.scan_time = scan->scan_time;
-    scan_ori.time_increment = scan->time_increment;
-    scan_ori.range_min = scan->range_min;
-    scan_ori.range_max = scan->range_max;
+	scan_ori.time_increment = scan->time_increment;
+	scan_ori.range_min = scan->range_min;
+	scan_ori.range_max = scan->range_max;
 	scan_ori.intensities = scan->intensities;
-    scan_ori.ranges = scan->ranges;
+	scan_ori.ranges = scan->ranges;
 }
 
 int main(int argc, char **argv)
@@ -22,9 +22,9 @@ int main(int argc, char **argv)
 	ros::init(argc, argv, "scan_publisher");
 	ros::NodeHandle nh;
 
-	ros::Subscriber measure_sub = nh.subscribe("/scan", 100, scanCallback);
-	ros::Publisher scan_pub = nh.advertise<sensor_msgs::LaserScan>("/scan_revised", 100);
-	
+	ros::Subscriber measure_sub = nh.subscribe("/scan_ori", 1000, scanCallback);
+	ros::Publisher scan_pub = nh.advertise<sensor_msgs::LaserScan>("/scan", 1000);
+	// ros::Rate loop_rate(50);
 	while(ros::ok())
 	{
 		sensor_msgs::LaserScan scan_revised;
@@ -39,7 +39,7 @@ int main(int argc, char **argv)
 		scan_revised.intensities = scan_ori.intensities;
 		scan_revised.ranges = scan_ori.ranges;
 		scan_pub.publish(scan_revised);
-		// ros::spinOnce();
+		ros::spinOnce();
 		// loop_rate.sleep();
 	}
 	return 0;
