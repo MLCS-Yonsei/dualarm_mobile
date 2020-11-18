cb9cf8f8a2a6e81edde268de0aab4a55017e21cc
Jaehyun Lim <jaehyunlim@yonsei.ac.kr>
2020-11-18 19:25:51 +0900

    Add lowpass filter to cmd_vel_publisher.cpp

--- dualarm_mobile_bringup/src/cmd_vel_publisher.cpp
+++ dualarm_mobile_bringup/src/cmd_vel_publisher.cpp
@@ -4,12 +4,13 @@
 bool isInitialized = false;
 ros::Publisher rpm_pub;
 ethercat_test::vel rpm_msg;
+int smoothing_factor
+
+int rpm_ref[4] = {0, 0, 0, 0};
 
 
 void cmdCallback(const geometry_msgs::Twist& cmd_vel)
 {
-
-  int rpm[4] = {0, 0, 0, 0};
 
   double u1 = cmd_vel.linear.x;
   double u2 = cmd_vel.linear.y;
@@ -27,16 +28,34 @@ void cmdCallback(const geometry_msgs::Twist& cmd_vel)
       u2 *= scaleFactor;
       u3 *= scaleFactor;
     }
-    rpm[0] = int( paramIK * (u1 - u2 - u3));
-    rpm[1] = int(-paramIK * (u1 + u2 + u3));
-    rpm[2] = int(-paramIK * (u1 - u2 + u3));
-    rpm[3] = int( paramIK * (u1 + u2 - u3));
+    rpm_ref[0] = int( paramIK * (u1 - u2 - u3));
+    rpm_ref[1] = int(-paramIK * (u1 + u2 + u3));
+    rpm_ref[2] = int(-paramIK * (u1 - u2 + u3));
+    rpm_ref[3] = int( paramIK * (u1 + u2 - u3));
   }
+
+  // if (isInitialized)
+  // {
+  //   for (unsigned int idx = 0; idx < 4; ++idx)
+  //     rpm_msg.velocity[idx] = rpm[idx];
+  //   rpm_pub.publish(rpm_msg);
+  // }
+}
+
+void encoderCallback(const ethercat_test::vel& msg)
+{
+
+  int rpm[4] = {0, 0, 0, 0};
+
+  rpm[0] =  msg.velocity[0];
+  rpm[1] = -msg.velocity[1];
+  rpm[2] = -msg.velocity[2];
+  rpm[3] =  msg.velocity[3];
 
   if (isInitialized)
   {
     for (unsigned int idx = 0; idx < 4; ++idx)
-      rpm_msg.velocity[idx] = rpm[idx];
+      rpm_msg.velocity[idx] = (smoothing_factor * rpm_ref[idx] + (10 - smoothing_factor) * rpm[idx]) / 10;
     rpm_pub.publish(rpm_msg);
   }
 }
@@ -49,6 +68,13 @@ int main(int argc, char **argv)
 
   ros::NodeHandle nh;
 
+  nh.param<int>("smoothing_factor", smoothing_factor, 10);
+  if (smoothing_factor > 10) {
+    smoothing_factor = 10;
+  }
+  else if (smoothing_factor < 1) {
+    smoothing_factor = 1;
+  }
   rpm_pub = nh.advertise<ethercat_test::vel>("/input_msg", 1);
   ros::Subscriber cmd_vel_sub = nh.subscribe("/cmd_vel", 1, cmdCallback);
   isInitialized = true;
