9d28f9f94f83f4c2551541b68a8ca03626fd926e
Jaehyun Lim <jaehyunlim@yonsei.ac.kr>
2020-11-27 11:26:29 +0900

    Update odom_publisher.cpp

--- dualarm_mobile_bringup/src/odom_publisher.cpp
+++ dualarm_mobile_bringup/src/odom_publisher.cpp
@@ -1,5 +1,6 @@
 #include <dualarm_mobile_bringup/publisher.h>
-
+#include <teb_local_planner/FeedbackMsg.h>
+#include <teb_local_planner/TrajectoryPointMsg.h>
 
 int rate;
 
@@ -19,18 +20,99 @@ ethercat_test::vel encoder;
 
 ros::Time currentTime;
 
-double frontLeft = 0.0;
-double frontRight = 0.0;
-double rearRight = 0.0;
-double rearLeft = 0.0;
+double linear_vel_x = 0.0;
+double linear_vel_y = 0.0;
+double angular_vel_z = 0.0;
+
+bool isInitialized = false;
+ros::Publisher rpm_pub;
+ethercat_test::vel rpm_msg;
+int smoothing_factor;
+
+//int rpm_ref[4] = {0, 0, 0, 0};
+//int rpm[4] = {0, 0, 0, 0};
+TrajectoryPointMsg ref[2];
+double startTime = 0.0;
+
+
+void trajCallback(const FeedbackMsg::ConstPtr& feedback)
+{
+  
+  startTime = feedback->header.stamp.toSec();
+  double t = ros::Time::now().toSec() - startTime;
+  unsigned int selected_traj = feedback->selected_trajectory_idx;
+  unsigned int end_idx = 1;
+  while (t > feedback->trajectories[selected_traj].trajectory[end_idx].time_from_start.toSec())
+    ++end_idx;
+  }
+  ref[0] = feedback->trajectories[selected_traj].trajectory[end_idx-1]; 
+  ref[1] = feedback->trajectories[selected_traj].trajectory[end_idx];
+  
+}
 
 
 void encoderCallback(const ethercat_test::vel& msg)
 {
-  frontLeft  =  double(msg.velocity[0]);
-  frontRight = -double(msg.velocity[1]);
-  rearRight  = -double(msg.velocity[2]);
-  rearLeft   =  double(msg.velocity[3]);
+
+  double frontLeft  =  double(msg.velocity[0]);
+  double frontRight = -double(msg.velocity[1]);
+  double rearRight  = -double(msg.velocity[2]);
+  double rearLeft   =  double(msg.velocity[3]);
+
+  linear_vel_x  = paramFKLinear  * (frontRight + frontLeft + rearRight + rearLeft);
+  linear_vel_y  = paramFKLinear  * (frontRight - frontLeft - rearRight + rearLeft);
+  angular_vel_z = paramFKAngular * (frontRight - frontLeft + rearRight - rearLeft);
+
+  double t = ros::Time::now().toSec() - startTime;
+  double dt = ref[1].time_from_start.toSec() - ref[0].time_from_start.toSec();
+
+  double a = t / dt;
+  if (a < 0)
+    a = 0.0;
+  else if (a > 1)
+    a = 1.0;
+  double b = 1.0 - a;
+
+  double u1 = a * ref[1].velocity.linear.x + b * ref[0].velocity.linear.x;
+  double u2 = a * ref[1].velocity.linear.y + b * ref[0].velocity.linear.y;
+  double u3 = a * ref[1].velocity.angular.z + b * ref[0].velocity.angular.z;
+  double err_u1 = u1 - linear_vel_x;
+  double err_u2 = u2 - linear_vel_y;
+  double err_u3 = u3 - angular_vel_z;
+  if (abs(u1) + abs(u2) + abs(u3) > 0.1)
+  {
+    u1 -= Kd_1 * err_u1;
+    u2 -= Kd_2 * err_u2;
+    u3 -= Kd_3 * err_u3;
+  }
+
+  u3 *= wheelSepearation;
+  double normDesired = abs(u1) + abs(u2) + abs(u3);
+  if (normDesired > 0.0001)
+  {
+    if (normDesired > normLimit)
+    {
+      double scaleFactor = normLimit / normDesired;
+      u1 *= scaleFactor;
+      u2 *= scaleFactor;
+      u3 *= scaleFactor;
+    }
+    rpm_msg.velocity[0] = int( paramIK * (u1 - u2 - u3));
+    rpm_msg.velocity[1] = int(-paramIK * (u1 + u2 + u3));
+    rpm_msg.velocity[2] = int(-paramIK * (u1 - u2 + u3));
+    rpm_msg.velocity[3] = int( paramIK * (u1 + u2 - u3));
+  }
+  else
+  {
+    for (unsigned int idx = 0; idx < 4; ++idx)
+      rpm_msg.velocity[idx] = 0;
+  }
+
+  if (isInitialized)
+  {
+    rpm_pub.publish(rpm_msg);
+  }
+
 }
 
 
@@ -52,8 +134,11 @@ int main(int argc, char **argv)
 
   ros::Rate loop_rate(rate);
 
+  rpm_pub = nh.advertise<ethercat_test::vel>("/input_msg", 1);
   ros::Publisher odom_pub = nh.advertise<nav_msgs::Odometry>(odom_topic, 10);
   ros::Subscriber encoder_sub = nh.subscribe(encoder_topic, 1, encoderCallback);
+  ros::Subscriber traj_sub = nh.subscribe("/move_base/TebLocalPlannerROS/teb_feedback", 1, trajCallback);
+  isInitialized = true;
 
   transform.setIdentity();
   if (listen_tf)
@@ -81,10 +166,6 @@ int main(int argc, char **argv)
     }
     else
     {
-      double linear_vel_x  = paramFKLinear  * (frontRight + frontLeft + rearRight + rearLeft);
-      double linear_vel_y  = paramFKLinear  * (frontRight - frontLeft - rearRight + rearLeft);
-      double angular_vel_z = paramFKAngular * (frontRight - frontLeft + rearRight - rearLeft);
-
       currentTime = ros::Time::now();
 
       double stepTime = currentTime.toSec() - odom.header.stamp.toSec();
